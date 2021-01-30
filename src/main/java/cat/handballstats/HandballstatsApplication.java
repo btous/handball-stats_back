9ec804beb44252ec05743c231c741afee1da4dde
Bernat <bernat.tous@gmail.com>
2021-01-30 13:36:48 +0100

    allow all CORS

--- src/main/java/cat/handballstats/HandballstatsApplication.java
+++ src/main/java/cat/handballstats/HandballstatsApplication.java
@@ -25,7 +25,7 @@ public class HandballstatsApplication {
 		CorsConfiguration config = new CorsConfiguration();
 		config.setAllowCredentials(true);
 		// *** URL below needs to match the Vue client URL and port ***
-		config.setAllowedOrigins(Collections.singletonList("*"));
+		config.setAllowedOrigins(Collections.singletonList(""));
 		config.setAllowedMethods(Collections.singletonList("*"));
 		config.setAllowedHeaders(Collections.singletonList("*"));
 		source.registerCorsConfiguration("/**", config);
